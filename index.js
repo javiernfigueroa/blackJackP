0da0193985a00d2529ca98516c073a218b83ac28
Javier Figueroa <109968115+javiernfigueroa@users.noreply.github.com>
2023-03-14 18:23:15 -0300

    Nuevo Boton Pedir Carta

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
-let firstCard = 10
-let secondCard = 11
-let sum = firstCard + secondCard
-let hasBlackJack = false
-let isAlive = true
-let message = ""
+let firstCard = 10;
+let secondCard = 11;
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+const HEADER_TEXT = document.getElementById("message-el")
+const SUM_ELE = document.querySelector("#sum-el")
+const CARDS_ELE = document.querySelector("#cards-el")
 
-message = "We are logging out";
+function startGame() {
+  SUM_ELE.innerText = "Sum: " + sum;
+  CARDS_ELE.innerText = 'Tus cartas: '+firstCard + ' - ' + secondCard
+  if (sum <= 20) {
+    message = "Quieres pedir una nueva carta? 🙂";
+  } else if (sum === 21) {
+    message = "Wohoo! Hiciste un Blackjack! 🥳";
+    hasBlackJack = true;
+  } else {
+    message = "Perdiste, mejor suerte la proxima! 😭";
+    isAlive = false;
+  }
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂"
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game! 😭"
-    isAlive = false
+  HEADER_TEXT.innerText = message;
 }
 
-console.log(message)
+function newCard() {
+    console.log("sacando carta")
+}
